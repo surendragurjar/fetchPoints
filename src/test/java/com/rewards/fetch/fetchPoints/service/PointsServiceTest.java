2707aa06db10eb33f71a906f9dd466f496e1c038
surendragurjar <surendragurjar@gmail.com>
2021-02-01 22:37:54 -0600

    added test cases
    test cases

--- src/test/java/com/rewards/fetch/fetchPoints/service/PointsServiceTest.java
+++ src/test/java/com/rewards/fetch/fetchPoints/service/PointsServiceTest.java
@@ -1,17 +1,16 @@
 package com.rewards.fetch.fetchPoints.service;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.rewards.fetch.fetchPoints.constants.PointsConstanst;
+import com.rewards.fetch.fetchPoints.exception.PointsException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,21 +18,42 @@ public class PointsServiceTest {
 
 	@Autowired
 	private PointsService pointsServiceImpl;
-	
 
 	@Test
-	public void testAddPoinsMethod() {
-		String result =pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
-		
+	public void testAddPointsMethod() {
+		String result = pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
 		assertEquals("Success", result);
 	}
 	
 	@Test
-	public void testDeductPoinsMethod() {
+	public void testAddPoints_LowBalanceException() {
 		pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
-		String result =pointsServiceImpl.duductPoints("Surendra", 300);
-		
+		Throwable exception = assertThrows(PointsException.class,
+				() -> pointsServiceImpl.addPoints("Surendra", "Marianos", -8000));
+		assertEquals(PointsConstanst.LOW_BALANCE_MESSAGE, exception.getMessage());
+	}
+
+	@Test
+	public void testDeductPointsMethod() {
+		pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
+		String result = pointsServiceImpl.duductPoints("Surendra", 300);
+
 		assertEquals("Success", result);
+	}
+
+	@Test
+	public void testDeductPoints_LowBalanceException() {
+		pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
+		Throwable exception = assertThrows(PointsException.class,
+				() -> pointsServiceImpl.duductPoints("Surendra", 8000));
+		assertEquals(PointsConstanst.LOW_BALANCE_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testDeductPoints_NegativePointsException() {
+		Throwable exception = assertThrows(PointsException.class,
+				() -> pointsServiceImpl.duductPoints("Surendra", -800));
+		assertEquals(PointsConstanst.NEGATIVE_POINTS_MESSAGE, exception.getMessage());
 	}
 
 }
