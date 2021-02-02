714308692a4f2f40ac4c78452f9b10d0def400ff
surendragurjar <surendragurjar@gmail.com>
2021-02-01 22:12:55 -0600

    test cases
    test case

--- src/test/java/com/rewards/fetch/fetchPoints/service/PointsServiceTest.java
+++ src/test/java/com/rewards/fetch/fetchPoints/service/PointsServiceTest.java
@@ -1,19 +1,39 @@
 package com.rewards.fetch.fetchPoints.service;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PointsServiceTest {
-	
+
 	@Autowired
-	private PointsServiceImpl pointsService;
+	private PointsService pointsServiceImpl;
 	
+
 	@Test
 	public void testAddPoinsMethod() {
+		String result =pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
 		
-		pointsService.addPoints("Surendra", "Marianos", 500);
-		System.out.println(pointsService.lstTransactions.size());
+		assertEquals("Success", result);
+	}
+	
+	@Test
+	public void testDeductPoinsMethod() {
+		pointsServiceImpl.addPoints("Surendra", "Marianos", 500);
+		String result =pointsServiceImpl.duductPoints("Surendra", 300);
+		
+		assertEquals("Success", result);
 	}
 
 }
